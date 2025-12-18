glycoverse/glycoverse

commit d6dbf0a5fe3279c14a5225f9ae492ec061f808c0

fix: fix the bug in glycoverse_deps
Fix the bug that `glycoverse_deps()` raises an error when `recursive` is TRUE.

R/update.R
@@ -183,7 +183,7 @@ glycoverse_deps <- function(recursive = FALSE, repos = getOption("repos")) {
       if (is.na(up) || up == "") {
         FALSE
       } else {
-        package_version(up) > loc
+        numeric_version(up) > loc
       }
     }
   )
@@ -209,7 +209,7 @@ safe_package_version <- function(pkg) {
   if (rlang::is_installed(pkg)) {
     utils::packageVersion(pkg)
   } else {
-    package_version("0")
+    package_version("0.0.0")
   }
 }
 

commit 6fb636689f9fd509707135f52828aca5fe1b9318

fix: fix the bug in glycoverse_sitrep
This bug was introduced in d6dbf0a that upstream versions of glycoverse packages cannot be fetched.

DESCRIPTION
@@ -51,7 +51,11 @@ Remotes:
     glycoverse/glymotif@*release,
     glycoverse/glyread@*release,
     glycoverse/glystats@*release,
-    glycoverse/glyvis@*release
+    glycoverse/glyvis@*release,
+    glycoverse/glydraw@*release,
+    glycoverse/glydb@*release,
+    glycoverse/glyanno@*release,
+    glycoverse/glysmith@*release
 Depends: 
     R (>= 4.1)
 VignetteBuilder: knitr

R/update.R
@@ -278,6 +278,9 @@ github_resolve_ref <- function(repo, ref) {
     return("main")
   }
 
+  old <- options(HTTPUserAgent = "glycoverse (https://github.com/glycoverse/glycoverse)")
+  on.exit(options(old), add = TRUE)
+
   if (identical(ref, "*release")) {
     url <- sprintf("https://api.github.com/repos/%s/releases/latest", repo)
     release <- suppressWarnings(
@@ -296,7 +299,15 @@ github_resolve_ref <- function(repo, ref) {
 }
 
 github_description_version <- function(repo, ref) {
+  if (is.na(ref) || ref == "") {
+    return(NA_character_)
+  }
+
   url <- sprintf("https://raw.githubusercontent.com/%s/%s/DESCRIPTION", repo, ref)
+
+  old <- options(HTTPUserAgent = "glycoverse (https://github.com/glycoverse/glycoverse)")
+  on.exit(options(old), add = TRUE)
+
   lines <- suppressWarnings(
     tryCatch(
       readLines(url, warn = FALSE),